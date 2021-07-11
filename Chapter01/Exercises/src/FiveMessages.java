e27ee8b804e0ebbe5d502a7eb8e7b96c4540b611
rjhefley <hefley.ryanj@gmail.com>
2021-07-10 23:00:22 -0500

    Added the following: ThreeMessages.java JavaPattern.java FiveMessages.java ComputeExpressions.java

---
+++ Chapter01/Exercises/src/FiveMessages.java
@@ -0,0 +1,9 @@
+public class FiveMessages {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Java.");
+    }
+}
